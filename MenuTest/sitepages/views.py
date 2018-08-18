06f9f6446f34631498b63df0baf40912abf03254
Liam-Cafearo <lcafearo@gmail.com>
2018-08-18 13:44:28 +0100

    updated page information to reflect challenge requirements

--- MenuTest/sitepages/views.py
+++ MenuTest/sitepages/views.py
@@ -5,21 +5,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import datetime
 
 
 def get_home(request):
-    return render(request, "sitepages/home.html",
-                  {"variable_a": "Welcome to the Home page!",
-                   "variable_b": "The home page has been rendered in the child template"})
+    now = datetime.datetime.now()
+    return render(request, "sitepages/home.html", {"current_date": now})
 
 
 def get_about(request):
     return render(request, "sitepages/about.html",
-                  {"variable_c": "Welcome to the about page",
-                   "variable_d": "The about page has been rendered in the child template"})
+                  {"variable_a": "Reiciendis molestiae rem. Architecto error aspernatur deserunt qui. Dolores dolor delectus occaecati dolores. Repellat perspiciatis ratione dicta consequatur. Earum nisi commodi animi. Modi inventore possimus quae eligendi.",
+                   "variable_b": "Dolores esse sint. Dolor quam magni ut non hic eum beatae. Asperiores ut recusandae molestias eligendi dolorem nobis non quo. Voluptates molestiae minima quaerat qui officiis temporibus autem voluptatem totam. Id maxime cupiditate ut reprehenderit rerum."})
 
 
 def get_contact(request):
     return render(request, "sitepages/contact.html",
-                  {"variable_e": "Welcome to the contact page",
-                   "variable_f": "The contact page has been rendered in the child template"})
+                  {"variable_c": "Firstname: Filiberto",
+                   "variable_d": "Lastname: Bruen",
+                   "variable_e": "Telephone Number: 1-501-780-2999 x27440"})
